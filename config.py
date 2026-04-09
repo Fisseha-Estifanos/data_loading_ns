74c56a578f2de016f8553f9549651c0127f4c784
fisseha-estifanos <fisseha.137@gmail.com>
2026-04-09 14:20:35 +0300

    minor mod

--- config.py
+++ config.py
@@ -7,21 +7,20 @@ All secrets should ideally come from environment variables in production.
 
 import os
 
-# --- NetSuite Account ---
-# FROM POSTMAN: https://4874529-sb3.suitetalk.api.netsuite.com/services/rest/record/v1/customer
-ACCOUNT_ID = "4874529-sb3"
-BASE_URL = f"https://{ACCOUNT_ID}.suitetalk.api.netsuite.com/services/rest/record/v1"
-SUITEQL_URL = (
-    f"https://{ACCOUNT_ID}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql"
-)
-
 # --- OAuth 1.0 Credentials ---
 # Replace these with your actual values, or set them as environment variables.
-CONSUMER_KEY = os.environ.get("NS_CONSUMER_KEY", "YOUR_CONSUMER_KEY")
-CONSUMER_SECRET = os.environ.get("NS_CONSUMER_SECRET", "YOUR_CONSUMER_SECRET")
-ACCESS_TOKEN = os.environ.get("NS_ACCESS_TOKEN", "YOUR_ACCESS_TOKEN")
-TOKEN_SECRET = os.environ.get("NS_TOKEN_SECRET", "YOUR_TOKEN_SECRET")
-REALM = os.environ.get("NS_REALM", ACCOUNT_ID)
+CONSUMER_KEY = os.environ.get("NS_CONSUMER_KEY", "")
+CONSUMER_SECRET = os.environ.get("NS_CONSUMER_SECRET", "")
+ACCESS_TOKEN = os.environ.get("NS_ACCESS_TOKEN", "")
+TOKEN_SECRET = os.environ.get("NS_TOKEN_SECRET", "")
+REALM = os.environ.get("NS_REALM", "")
+
+# --- NetSuite Account ---
+# FROM POSTMAN: https://4874529-sb3.suitetalk.api.netsuite.com/services/rest/record/v1/customer
+BASE_URL = f"https://{REALM}.suitetalk.api.netsuite.com/services/rest/record/v1"
+SUITEQL_URL = (
+    f"https://{REALM}.suitetalk.api.netsuite.com/services/rest/query/v1/suiteql"
+)
 
 # --- Data File Paths ---
 CUSTOMERS_CSV = "data/customers-kleene-export-2026-04-09.csv"
