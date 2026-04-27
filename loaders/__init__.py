b7cf4601f6d9cddb59a8e152a3be3ecbc210fb53
fisseha-estifanos <fisseha.137@gmail.com>
2026-04-27 23:00:58 +0300

    feat: implement subscription pricing retrofit script and loaders to manage data reconciliation updates

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,11 +1,13 @@
 from loaders.customer import CustomerLoader
 from loaders.billing_account import BillingAccountLoader
+from loaders.price_plan import PricePlanLoader
 from loaders.subscription import SubscriptionLoader
 from loaders.one_off import OneOffLoader
 
 __all__ = [
     "CustomerLoader",
     "BillingAccountLoader",
+    "PricePlanLoader",
     "SubscriptionLoader",
     "OneOffLoader",
 ]
