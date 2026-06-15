04ca66d692043012688e805b685bdda8a77e4348
fisseha-estifanos <fisseha.137@gmail.com>
2026-06-15 16:24:08 +0300

    fix: update billing CSV file path to remapped version

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ SUITEQL_URL = (
 # --- Data File Paths ---
 CUSTOMERS_CSV = "data/customers-kleene-export-2026-06-02.csv"
 
-BILLING_CSV = "data/billing-accounts-kleene-export-2026-06-15.csv"
+BILLING_CSV = "data/billing-accounts-kleene-export-2026-06-15-remapped.csv"
 PRICE_PLANS_CSV = "data/pricing-json-kleene-export-2026-06-11.csv"
 SUBSCRIPTIONS_CSV = "data/subs-kleene-export-2026-06-15.csv"
 
